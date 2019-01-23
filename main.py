1a5c2307f25640ec40efcb8e37788c0f25cc7afe
dheerajkumar1210 <dheeraj.kumar7@gmail.com>
2019-01-23 22:36:28 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 23 21:48:26 2019
+
+@author: dheeraj_kumar
+"""
+
+
+#song name
+def Song():
+    return "Be-Sabab Baat"
+
+#singer
+def Artist():
+    return "Jagjit Singh"
+
+#date published
+def Year():
+    return 1994
+    
+def IsAlbumNameKnown():
+    return True
+    
+def IsPartySong():
+    return False
+
+print(Song())
+print(Artist())
+print(Year())
+print(IsAlbumNameKnown())
+print(IsPartySong())
